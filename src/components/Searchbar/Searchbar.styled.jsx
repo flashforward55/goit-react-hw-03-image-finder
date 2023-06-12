79ef7642df979d873ca43c2512c99d132821399e
Anton Styazhkin <rudana089@gmail.com>
2023-06-12 15:59:42 +0300

    Update Searchbar.styled.jsx

--- src/components/Searchbar/Searchbar.styled.jsx
+++ src/components/Searchbar/Searchbar.styled.jsx
@@ -1,14 +1,18 @@
 import styled from 'styled-components';
 // Компонент Searchbar
 export const SearchbarContainer = styled.header`
-  position: sticky;
   top: 0;
   left: 0;
+  position: sticky;
+  z-index: 1100;
   display: flex;
   justify-content: center;
   align-items: center;
   min-height: 64px;
-  padding: 12px 24px;
+  padding-right: 24px;
+  padding-left: 24px;
+  padding-top: 12px;
+  padding-bottom: 12px;
   color: #fff;
   background-color: #3f51b5;
   box-shadow: 0px 2px 4px -1px rgba(0, 0, 0, 0.2),
@@ -30,7 +34,7 @@ export const SearchButton = styled.button`
   width: 48px;
   height: 48px;
   border: 0;
-  background-image: url('https://image.flaticon.com/icons/svg/149/149852.svg');
+  background-image: url('https://cdn-icons-png.flaticon.com/512/109/109164.png');
   background-size: 40%;
   background-repeat: no-repeat;
   background-position: center;
@@ -51,7 +55,8 @@ export const SearchInput = styled.input`
   font-size: 20px;
   border: none;
   outline: none;
-  padding: 4px;
+  padding-left: 4px;
+  padding-right: 4px;
 
   &::placeholder {
     font: inherit;
