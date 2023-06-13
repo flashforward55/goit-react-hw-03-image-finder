1a9186342fde5cc96de75418e02b50de6dba63f9
Anton Styazhkin <rudana089@gmail.com>
2023-06-13 15:06:12 +0300

    Update Searchbar.jsx

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,4 +1,3 @@
-import { Component } from 'react';
 import PropTypes from 'prop-types';
 import {
   SearchbarContainer,
@@ -8,28 +7,24 @@ import {
   SearchInput,
 } from './Searchbar.styled';
 
-class Searchbar extends Component {
-  render() {
-    const { onSubmit, isLoading } = this.props;
-
-    return (
-      <SearchbarContainer>
-        <SearchForm onSubmit={onSubmit}>
-          <SearchButton type="submit" disabled={isLoading}>
-            <SearchButtonLabel>Search</SearchButtonLabel>
-          </SearchButton>
-          <SearchInput
-            type="text"
-            name="searchQuery"
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-          />
-        </SearchForm>
-      </SearchbarContainer>
-    );
-  }
-}
+const Searchbar = ({ onSubmit, isLoading }) => {
+  return (
+    <SearchbarContainer>
+      <SearchForm onSubmit={onSubmit}>
+        <SearchButton type="submit" disabled={isLoading}>
+          <SearchButtonLabel>Search</SearchButtonLabel>
+        </SearchButton>
+        <SearchInput
+          type="text"
+          name="searchQuery"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+        />
+      </SearchForm>
+    </SearchbarContainer>
+  );
+};
 
 Searchbar.propTypes = {
   onSubmit: PropTypes.func.isRequired,
